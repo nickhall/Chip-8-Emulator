686b20b75ee6acff5d9ea8895641f85188c5c731
Nick Hall <iamssb@gmail.com>
2015-03-14 17:51:23 +0900

    Up to loading ROM into memory

--- Chippy/Chippy/chip8.cpp
+++ Chippy/Chippy/chip8.cpp
@@ -1,5 +1,6 @@
+//#include <iostream>
+//#include <fstream>
 #include "chip8.h"
-#include <iostream>
 
 chip8::chip8()
 {
@@ -9,7 +10,43 @@ chip8::chip8()
 
 void chip8::init()
 {
+	pc = 0x200;
 	std::cout << "Emulator initialized" << std::endl;
+	load();
+}
+
+void chip8::load()
+{
+	//TODO: Remove hard coded location
+	FILE *file = fopen("C:\\Users\\Nick\\Documents\\GitHub\\Chip-8-Emulator\\Chippy\\Debug\\PONG", "rb");
+	if (file != NULL)
+	{
+		cout << "File opened" << endl;
+		
+		// Determine file size
+		fseek(file, 0L, SEEK_END);
+		int fsize = ftell(file);
+		fseek(file, 0L, SEEK_SET);
+
+		// Allocate memory buffer. There's probably a  better way to do this but for now let's just get it working
+		unsigned char *buffer = (unsigned char*) malloc(fsize);
+		// Read file contents into buffer and close file
+		fread(buffer, fsize, 1, file);
+		fclose(file);
+
+		// Loop over each byte and add it into the memory array at the correct offset
+		for (int i = 0; i < fsize; ++i)
+		{
+			memory[0x200 + i] = buffer[i];
+			// Print for debug purposes
+			printf("%02X", memory[0x200 + i]);
+		}
+		cout << endl;
+	}
+	else
+	{
+		cout << "Couldn't find pong";
+	}
 }
 
 chip8::~chip8()
--- Chippy/Chippy/chip8.h
+++ Chippy/Chippy/chip8.h
@@ -1,7 +1,20 @@
 #pragma once
+#include <iostream>
+#include <fstream>
+using namespace std;
+
 class chip8
 {
 public:
+	chip8();
+	~chip8();
+	void init();
+	void pause();
+	void start();
+	void load();
+
+private:
+	//istream file;					// Input file
 	unsigned char memory[4096];		// Main memory
 	int pc;							// Program counter
 	int sp;							// Stack pointer
@@ -10,9 +23,5 @@ public:
 	unsigned char screen[32 * 64];	// Video buffer
 	unsigned short opcode;			// Opcode
 	unsigned char key[16];			// Keyboard
-
-	chip8();
-	void init();
-	~chip8();
+	//TODO: Sound and timers
 };
-
