671ed9304da629543b8627029efce48dc364f011
Nick Hall <iamssb@gmail.com>
2015-03-17 00:08:36 +0900

    SDL basics

--- Chippy/Chippy/Main.cpp
+++ Chippy/Chippy/Main.cpp
@@ -1,12 +1,42 @@
 #include <iostream>
+#include <SDL.h>
 #include "chip8.h"
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
 	chip8 emulator;
 	emulator.run();
 
+	//SDL_Init(SDL_INIT_EVERYTHING);
+
+	//The window we'll be rendering to
+	SDL_Window* gWindow = NULL;
+
+	//The surface contained by the window
+	SDL_Surface* gScreenSurface = NULL;
+
+	//The image we will load and show on the screen
+	SDL_Surface* gHelloWorld = NULL;
+	//Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+	}
+	else
+	{
+		//Create window
+		gWindow = SDL_CreateWindow("There's my chippy", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 320, SDL_WINDOW_SHOWN);
+		if (gWindow == NULL)
+		{
+			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+		}
+		else
+		{
+			//Get window surface
+			gScreenSurface = SDL_GetWindowSurface(gWindow);
+		}
+	}
 	// Update emulator
 	emulator.update(0);
 
