b9d588acd75c0694c03ec0805508c1256d0582f5
Nick Hall <iamssb@gmail.com>
2015-03-17 10:55:15 +0900

    Window close event and loop, drew a rectangle

--- Chippy/Chippy/Main.cpp
+++ Chippy/Chippy/Main.cpp
@@ -8,13 +8,11 @@ int main(int argc, char *argv[])
 	chip8 emulator;
 	emulator.run();
 
-	//SDL_Init(SDL_INIT_EVERYTHING);
-
 	//The window we'll be rendering to
-	SDL_Window* gWindow = NULL;
+	SDL_Window* Window = NULL;
 
 	//The surface contained by the window
-	SDL_Surface* gScreenSurface = NULL;
+	SDL_Surface* ScreenSurface = NULL;
 
 	//Initialize SDL
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
@@ -24,20 +22,49 @@ int main(int argc, char *argv[])
 	else
 	{
 		//Create window
-		gWindow = SDL_CreateWindow("There's my chippy", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 320, SDL_WINDOW_SHOWN);
-		if (gWindow == NULL)
+		Window = SDL_CreateWindow("There's my chippy", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 320, SDL_WINDOW_SHOWN);
+		if (Window == NULL)
 		{
 			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
 		}
 		else
 		{
 			//Get window surface
-			gScreenSurface = SDL_GetWindowSurface(gWindow);
+			ScreenSurface = SDL_GetWindowSurface(Window);
 		}
 	}
-	// Update emulator
-	emulator.update(0);
 
-	system("pause");
+	SDL_Renderer *renderer = SDL_CreateRenderer(Window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (renderer == nullptr)
+	{
+		SDL_DestroyWindow(Window);
+		std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+	bool quit = false;
+	SDL_Event e;
+	while (!quit)
+	{
+		while (SDL_PollEvent(&e))
+		{
+			if (e.type == SDL_QUIT)
+			{
+				quit = true;
+			}
+		}
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Clear to black
+		SDL_RenderClear(renderer);
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // Draw white
+		SDL_Rect rect;
+		rect.x = 50;
+		rect.y = 50;
+		rect.h = 50;
+		rect.w = 50;
+		SDL_RenderFillRect(renderer, &rect);
+		SDL_RenderPresent(renderer);
+		emulator.update(0);
+	}
+	SDL_Quit();
 	return 0;
 }
